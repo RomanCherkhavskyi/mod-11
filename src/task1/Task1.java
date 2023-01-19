6e51775e1d21e5644ec4d756636ae2d215fa09ee
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-01-19 15:29:47 +0200

    Change code from task 1 and task 2

--- src/task1/Task1.java
+++ src/task1/Task1.java
@@ -3,7 +3,6 @@ package task1;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Task1 {
 
@@ -13,20 +12,35 @@ public class Task1 {
         System.out.println("retOddNames(list) = " + retOddNames(list));
     }
 
-    public static String retOddNames(List<String> list){
-        return IntStream.range(0, list.size())
-                .boxed()
-                .map(number -> removeOdd(number, list))
+    //new code: used ternal operator in map
+    public static String retOddNames(List<String> names){
+        return names.stream().map((i) -> names.indexOf(i)%2 != 0 ? names.indexOf(i) + ". " + i + ", " : "")
                 .collect(Collectors.joining());
     }
 
-    public static String removeOdd(int number, List<String> list){
-        return number % 2 != 0 ? number + "." + list.get(number) + " " : "";
-    }
+//old code
+//    public static String retOddNames(List<String> list){
+//        return IntStream.range(0, list.size())
+//                .boxed()
+//                .map(number -> removeOdd(number, list))
+//                .collect(Collectors.joining());
+//    }
+
+//    public static String removeOdd(int number, List<String> list){
+//        return number % 2 != 0 ? number + "." + list.get(number) + " " : "";
+//    }
 
 }
 
 /*
 Метод приймає на вхід список імен.
 Необхідно повернути рядок вигляду 1. Ivan, 3. Peter... лише з тими іменами, що стоять під непарним індексом (1, 3 тощо)
+ */
+
+
+/*
+public String filterNames1(List<String> names){
+        return names.stream().map((i) -> names.indexOf(i)%2 != 0 ? names.indexOf(i) + ". " + i + ", " : "")
+                .collect(Collectors.joining());
+    }
  */
--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -1,33 +1,45 @@
 package task2;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Task2 {
 
     public static void main(String[] args) {
         List<String> list = List.of(new String[]{"Ivan", "Yuri", "Roman", "Nazar", "Stepan", "Max", "Igor"});
 
-        Iterator<String> iterator;
+        System.out.println("sortedList(list) = " + sortedList(list));
 
-        Stream<String> streamSorted =
-                list.stream()
-                        .map(String::toUpperCase)
-                        .sorted(Comparator.reverseOrder());
 
-        iterator = streamSorted.iterator();
-        System.out.print("streamSorted = ");
-        while (iterator.hasNext())
-            System.out.print(iterator.next()+" ");
+        //old code
+//        Iterator<String> iterator;
+//
+//        Stream<String> streamSorted =
+//                list.stream()
+//                        .map(String::toUpperCase)
+//                        .sorted(Comparator.reverseOrder());
+//
+//        iterator = streamSorted.iterator();
+//        System.out.print("streamSorted = ");
+//        while (iterator.hasNext())
+//            System.out.print(iterator.next()+" ");
+
     }
+
+    //newcode without iterator. Used method's toUpperCase and reverseOrder
+    public static List<String> sortedList(List<String> names){
+        return names.stream().map(String::toUpperCase).sorted(Comparator.reverseOrder()).toList();
+    }
+
 }
-
-
-
 
 /*
 Метод приймає на вхід список рядків (можна взяти список із Завдання 1).
 Повертає список цих рядків у верхньому регістрі, і відсортованих за спаданням (від Z до A).
+ */
+
+/*
+public List<String> sortedList(List<String> names){
+        return names.stream().map(String::toUpperCase).sorted(Comparator.reverseOrder()).toList());
+    }
  */
